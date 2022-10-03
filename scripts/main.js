e836d8fc11a392e1cc9db365ebe6461d72aa1054
Kon10t <101184174+Kon10t@users.noreply.github.com>
2022-10-03 11:47:54 +0300

    NEW

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,6 @@ function loadDoc() {
     const xhttp = new function() {
         document.getElementById("demo").innerHTML = this.responseText;
     }
-    xhttp.open("GET", "ajax_text.txt");
+    xhttp.open("POST", "ajax_text.txt");
     xhttp.send();
 }
